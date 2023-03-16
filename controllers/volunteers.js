f359579c9f0c29a1235efd6bf9f59a3212d6eb45
romainfrezier <romainfrezier@gmail.com>
2023-03-16 14:58:40 +0100

    volunteers.js controller (need to verify)

---
+++ controllers/volunteers.js
@@ -0,0 +1,105 @@
+const Volunteers = require('../models/Volunteers');
+const Slots = require('../models/Slots');
+
+exports.createVolunteer = (req, res, next) => {
+    const volunteer = new Volunteers({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        firebaseId: req.body.firebaseId,
+        isAdmin: false,
+        festival: req.body.festival,
+    });
+    volunteer.save().then(
+        () => {
+            res.status(201).json({
+                message: 'Volunteer saved successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+}
+
+exports.updateVolunteer = (req, res, next) => {
+    Volunteers.findOneAndUpdate({_id: req.params.id}, {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        festival: req.body.festival,
+        // Si il y a un slot, alors on ajoute l'id bu bénévole dans le tableau des bénévoles du slot
+        // Sinon, on ne fait rien
+        // TODO
+        slot: req.body.slot ? req.body.slot : null,
+        availableSlots: req.body.availableSlots
+    }).then(
+        () => {
+            res.status(201).json({
+                message: 'Volunteer updated successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+}
+
+exports.deleteVolunteer = (req, res, next) => {
+    Volunteers.deleteOne({_id: req.params.id}).then(
+        Slots.updateMany({volunteers: req.params.id}, {$pull: {volunteers: req.params.id}}).then(
+        () => {
+            res.status(200).json({
+                message: 'Volunteer deleted successfully!'
+            });
+        }).catch(
+            (error) => {
+                res.status(400).json({
+                    error: error
+                });
+            }
+        )
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    )
+}
+
+exports.getOneVolunteer = (req, res, next) => {
+    Volunteers.findOne({
+        _id: req.params.id
+    }).then(
+        (volunteer) => {
+            res.status(200).json(volunteer);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+}
+
+exports.getAllVolunteers = (req, res, next) => {
+    Volunteers.find().then(
+        (volunteers) => {
+            res.status(200).json(volunteers);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+}
--- models/volunteers.js
+++ models/volunteers.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const volunteerSchema = new mongoose.Schema({
-  first_name: {type: String,required: true},
-  last_name: {type: String, required: true},
+  firstName: {type: String,required: true},
+  lastName: {type: String, required: true},
   email: { type: String, required: true, unique: true },
   firebaseId: { type: String, required: true },
   festival: { type: String, required: true },
   isAdmin: { type: Boolean, required: true },
-  available_slots: [{
-    slot: { type: String, required: true },
+  availableSlots: [{
+    slot: { type: String, default: null },
     zone: { type: String, default: null },
   }],
 }, { collection : 'volunteers' });
